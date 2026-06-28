1110c4bd122603530ae73a3305c490dc6aedc4e9
juanpazs00 <122044165@upq.edu.mx>
2026-06-27 23:27:21 -0600

    forma de ruta (devolver un formato JSON)

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return 'Hola ETC';
+
+return ['aprendible' =>'.com'];
+
+//    return 'Hola ETC'; para retornar una cadena de caracteres basica
 //    return view('welcome'); para retornar una vista dentro del archivo
 });
